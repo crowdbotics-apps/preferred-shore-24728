f593d133348b36f16cbbd459bdcab5bf6f6951f3
Crowdbotics <team@crowdbotics.com>
2021-02-26 18:46:28 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/preferred_shore_24728/MainActivity.java
@@ -0,0 +1,15 @@
+package com.preferred_shore_24728;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "preferred_shore_24728";
+    }
+}
