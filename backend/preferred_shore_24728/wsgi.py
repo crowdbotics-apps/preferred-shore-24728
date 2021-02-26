b184f3caecc135307b55620b3f57e0f234491c68
Crowdbotics <team@crowdbotics.com>
2021-02-26 18:47:52 +0000

    Update models

--- backend/preferred_shore_24728/wsgi.py
+++ backend/preferred_shore_24728/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'preferred_shore_24728.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "preferred_shore_24728.settings")
 
 application = get_wsgi_application()
